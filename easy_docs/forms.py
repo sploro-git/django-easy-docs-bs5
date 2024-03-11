0a59dd313da0d7fce011dc703cd6a74ae259cef5
Iván Pajares Fuente <ivan.pajares@sploro.eu>
2024-03-11 16:20:59 +0100

    Sync fork

--- easy_docs/forms.py
+++ easy_docs/forms.py
@@ -7,13 +7,13 @@ class BaseDocumentationForm(forms.ModelForm):
     title = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), help_text="The title of this documentation.")
     content = MarkdownxFormField()
     keywords = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}), help_text="Comma separated list of keywords to help users find this documentation.", empty_value="Comma separated list.")
-    public = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'large-checkbox'}), help_text="Check this box to make this documentation public.")
+    public = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}), help_text="Check this box to make this documentation public.")
     regex_url = forms.CharField(
         max_length=200, required=False,
         widget=forms.TextInput(attrs={'class': 'form-control form-control-lg'}),
         label='Regex Pattern'
     )
-    
+
     class Meta:
         model = Documentation
         fields = ('title', 'content', 'keywords', 'public', 'regex_url')
